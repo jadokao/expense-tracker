58a3d44e7b95bff79bada06c7af5deff82452857
jadokao <jackykao528@outlook.com>
2021-08-24 22:14:20 +0800

    feat: insert CATEGORY object

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,52 +2,34 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/Record')
+const CATEGORY = require('../../models/category.js')
 
 // 首頁：顯示分錄
 router.get('/', (req, res) => {
-	Record.find()
-		.lean()
-		.then(records => {
-			res.render('index', { records })
-		})
-		.catch(error => console.error(error))
+  Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(record => {
+        switch (record.category) {
+          case '家居物業':
+            record['icon'] = CATEGORY.home
+            break
+          case '交通出行':
+            record['icon'] = CATEGORY.transportation
+            break
+          case '休閒娛樂':
+            record['icon'] = CATEGORY.entertainment
+            break
+          case '餐飲食品':
+            record['icon'] = CATEGORY.food
+            break
+          default:
+            record['icon'] = CATEGORY.other
+        }
+      })
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
 })
-
-// // 篩選分錄的功能
-// router.get('/:category', (req, res) => {
-// 	// 網址上所顯示：選取的分類
-// 	const categoryId = req.query.category
-// 	// 用來篩選分錄的item
-// 	const category = { category: req.query.category }
-
-// 	let categories = []
-// 	Category.find().lean().then(item => {
-// 		categories.push(...item)
-// 	})
-
-// 	return Record.find(category)
-// 		.lean()
-// 		.populate('category')
-// 		.then(records => {
-// 			let recordCategory = ''
-// 			if (records.length > 0) {
-// 				recordCategory = records[0].category._id.toString()
-// 			}
-
-// 			const home = compareTargetCategory(categories, '家居物業', recordCategory)
-// 			const transportation = compareTargetCategory(categories, '交通出行', recordCategory)
-// 			const entertainment = compareTargetCategory(categories, '休閒娛樂', recordCategory)
-// 			const food = compareTargetCategory(categories, '餐飲食品', recordCategory)
-// 			const other = compareTargetCategory(categories, '其他', recordCategory)
-
-// 			res.render('index', { records, categories, categoryId, home, transportation, entertainment, food, other })
-// 		})
-// 		.catch(error => console.error(error))
-// })
-
-// function compareTargetCategory (array, type, target) {
-// 	let founded = array.find(item => item.category.toString() === type)
-// 	return founded._id.toString() === target ? true : false
-// }
 
 module.exports = router
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -3,98 +3,143 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 const Record = require('../../models/Record')
+const CATEGORY = require('../../models/category.js')
 
 // 新增分錄的頁面
 router.get('/new', (req, res) => {
-	res.render('new')
+  res.render('new')
 })
 
 // 新增分錄的功能
 router.post('/', (req, res) => {
-	const { name, date, category, amount } = req.body
+  const { name, date, category, amount } = req.body
 
-	let recordLength = []
-	Record.find().lean().then(records => {
-		recordLength.push(records.length)
-		return Record.create({
-			id: Number(recordLength[0]) + 1,
-			name,
-			date,
-			category,
-			amount,
-		})
-			.then(() => {
-				res.redirect('/')
-			})
-			.catch(error => console.error(error))
-	})
+  Record.find().lean().then(records => {
+    let recordIcon = ''
+    records.forEach(record => {
+      switch (record.category) {
+        case '家居物業':
+          recordIcon = CATEGORY.home
+          break
+        case '交通出行':
+          recordIcon = CATEGORY.transportation
+          break
+        case '休閒娛樂':
+          recordIcon = CATEGORY.entertainment
+          break
+        case '餐飲食品':
+          recordIcon = CATEGORY.food
+          break
+        default:
+          recordIcon = CATEGORY.other
+      }
+    })
+
+    let recordLength = []
+    recordLength.push(records.length)
+    Record.create({
+      id: Number(recordLength[0]) + 1,
+      name,
+      date,
+      category,
+      amount,
+      icon: recordIcon,
+    })
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
 })
 
 // 編輯分錄的頁面
 router.get('/:id/edit', (req, res) => {
-	const id = req.params.id
-	return Record.findById(id)
-		.lean()
-		.then(record => {
-			const home = compareTargetCategory(record, '家居物業')
-			const transportation = compareTargetCategory(record, '交通出行')
-			const entertainment = compareTargetCategory(record, '休閒娛樂')
-			const food = compareTargetCategory(record, '餐飲食品')
-			const other = compareTargetCategory(record, '其他')
-
-			res.render('edit', { record, home, transportation, entertainment, food, other })
-		})
-		.catch(error => console.error(error))
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => {
+      res.render('edit', { record, CATEGORY })
+    })
+    .catch(error => console.error(error))
 })
 
 // 編輯分錄的功能
 router.put('/:id', (req, res) => {
-	const id = req.params.id
-	const { name, date, category, amount } = req.body
-	return Record.findById(id)
-		.then(record => {
-			record.name = name
-			record.date = date
-			record.category = category
-			record.amount = amount
-			return record.save()
-		})
-		.then(() => res.redirect(`/`))
-		.catch(error => console.log(error))
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      let recordIcon = ''
+      switch (record.category) {
+        case '家居物業':
+          recordIcon = CATEGORY.home
+          break
+        case '交通出行':
+          recordIcon = CATEGORY.transportation
+          break
+        case '休閒娛樂':
+          recordIcon = CATEGORY.entertainment
+          break
+        case '餐飲食品':
+          recordIcon = CATEGORY.food
+          break
+        default:
+          recordIcon = CATEGORY.other
+      }
+
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      record.icon = recordIcon
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
 })
 
 // 刪除分錄的功能
 router.delete('/:id', (req, res) => {
-	const id = req.params.id
-	return Record.findById(id)
-		.then(record => record.remove())
-		.then(() => res.redirect('/'))
-		.catch(error => console.log(error))
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // 篩選分錄的功能
 router.get('/:category', (req, res) => {
-	// 網址上所顯示：選取的分類
-	const categoryId = req.query.category
-	// 用來篩選分錄的item
-	const category = { category: req.query.category }
+  // 網址上所顯示：選取的分類
+  const categoryId = req.query.category
+  // 用來篩選分錄的item
+  const category = { category: req.query.category }
 
-	return Record.find(category)
-		.lean()
-		.then(records => {
-			const home = compareTargetCategory(records[0], '家居物業')
-			const transportation = compareTargetCategory(records[0], '交通出行')
-			const entertainment = compareTargetCategory(records[0], '休閒娛樂')
-			const food = compareTargetCategory(records[0], '餐飲食品')
-			const other = compareTargetCategory(records[0], '其他')
+  return Record.find(category)
+    .lean()
+    .then(records => {
+      records.forEach(record => {
+        switch (record.category) {
+          case '家居物業':
+            record['icon'] = CATEGORY.home
+            break
+          case '交通出行':
+            record['icon'] = CATEGORY.transportation
+            break
+          case '休閒娛樂':
+            record['icon'] = CATEGORY.entertainment
+            break
+          case '餐飲食品':
+            record['icon'] = CATEGORY.food
+            break
+          default:
+            record['icon'] = CATEGORY.other
+        }
+      })
 
-			res.render('index', { records, categoryId, home, transportation, entertainment, food, other })
-		})
-		.catch(error => console.error(error))
+      const firstRecord = records[0]
+      res.render('index', { records, firstRecord })
+    })
+    .catch(error => console.error(error))
 })
-
-function compareTargetCategory (object, type) {
-	return object.category === type ? true : false
-}
 
 module.exports = router
