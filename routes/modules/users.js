e7dd5a04a5a1c429a1502648f7e081de06450ee1
jadokao <jackykao528@outlook.com>
2021-09-06 17:21:48 +0800

    feat: add bcrypt to generate password

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 const User = require('../../models/user')
@@ -42,16 +43,17 @@ router.post('/register', (req, res) => {
   User.findOne({ email }).then(user => {
     if (user) {
       errors.push({ message: '這個 Email 已經註冊過了。' })
-      res.render('register', { errors, name, email, password, confirmPassword })
-    } else {
-      return User.create({
+      return res.render('register', { errors, name, email, password, confirmPassword })
+    }
+    return bcrypt.genSalt(10).then(salt => bcrypt.hash(password, salt)).then(hash => {
+      User.create({
         name,
         email,
-        password,
+        password: hash,
       })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
-    }
+    })
   })
 })
 
