bb515f3479ce4ba948fe6a081b4f53cd7a3f7e5d
jadokao <jackykao528@outlook.com>
2021-09-06 15:41:03 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 
 // 引用mongoose相關設定
@@ -21,6 +22,15 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: requ
 app.set('view engine', 'hbs')
 
 app.use(methodOverride('_method'))
+
+app.use(
+  session({
+    secret: 'trackerSecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+
 app.use(routes)
 
 app.listen(PORT, () => {
