062f44d4a86fbfc96860637a7315fa9709cc2f7f
jadokao <jackykao528@outlook.com>
2021-09-07 02:07:10 +0800

    feat: modify record and category seeders

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 
 const Category = require('../category')
@@ -5,10 +8,10 @@ const Category = require('../category')
 const categories = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
 const icons = ['fas fa-home', 'fas fa-shuttle-van', 'fas fa-grin-beam', 'fas fa-utensils', 'fas fa-pen']
 
-db.once('open', () => {
-  console.log('mongodb connected')
+db.once('open', async () => {
   for (let i = 0; i < categories.length; i++) {
-    Category.create({ category: categories[i], icon: icons[i] })
+    await Category.create({ name: categories[i], icon: icons[i] })
   }
   console.log('Categories done')
+  process.exit()
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,15 +1,94 @@
-// const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
 
 const Record = require('../Record')
-// mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
-// const db = mongoose.connection
+const User = require('../user')
+const Category = require('../category')
 
 const records = require('../../records.json')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+}
 
-db.once('open', () => {
-  console.log('mongodb connected!')
-  // insert a Array
-  Record.insertMany(records)
-  console.log('Records done')
+db.once('open', async () => {
+  // 取得各個category的mongodb id
+  let { home, transportation, entertainment, food, other } = ''
+  await Category.find().then(categories => {
+    categories.forEach(item => {
+      switch (item.name) {
+        case '家居物業':
+          home = item._id
+          break
+        case '交通出行':
+          transportation = item._id
+          break
+        case '休閒娛樂':
+          entertainment = item._id
+          break
+        case '餐飲食品':
+          food = item._id
+          break
+        default:
+          other = item._id
+      }
+    })
+  })
+
+  // 建立使用者資料
+  await bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(
+      async hash =>
+        await User.create({
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: hash,
+        })
+    )
+    .then(async user => {
+      const userId = user._id
+
+      await Promise.all(
+        records.map(async record => {
+          // 把原本的文字改成mongodb id
+          switch (record.category) {
+            case '家居物業':
+              record.category = home
+              break
+            case '交通出行':
+              record.category = transportation
+              break
+            case '休閒娛樂':
+              record.category = entertainment
+              break
+            case '餐飲食品':
+              record.category = food
+              break
+            default:
+              record.category = other
+          }
+
+          // 建立分錄資料
+          await Record.create({
+            id: record.id,
+            name: record.name,
+            date: record.date,
+            amount: record.amount,
+            category: record.category,
+            merchant: record.merchant,
+            userId,
+          })
+        })
+      )
+    })
+    .then(() => {
+      console.log('Records done')
+      process.exit()
+    })
 })
