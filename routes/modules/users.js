96421e8168dfbe174d445e60358552db55fdc44e
jadokao <jackykao528@outlook.com>
2021-09-06 15:19:50 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// 登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// 登入功能
+router.post('/login', (req, res) => {
+  res.render('/')
+})
+
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
