eb0318a0c2dde374b1dfb8c6f5b90cf47e451c74
jadokao <jackykao528@outlook.com>
2021-09-06 17:15:59 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,6 +15,7 @@ router.post(
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login',
+    failureFlash: true,
   })
 )
 
@@ -26,11 +27,22 @@ router.get('/register', (req, res) => {
 // 註冊功能
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
 
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('user already exists')
-      res.render('register', { name, email, password, confirmPassword })
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      res.render('register', { errors, name, email, password, confirmPassword })
     } else {
       return User.create({
         name,
@@ -46,6 +58,7 @@ router.post('/register', (req, res) => {
 // 登出功能
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出！')
   res.redirect('/users/login')
 })
 
