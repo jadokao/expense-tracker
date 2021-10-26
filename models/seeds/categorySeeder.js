8fa20cbe10184421c0ab01306b845c03fc70c8fd
jadokao <jackykao528@outlook.com>
2021-10-26 19:56:03 +0800

    change category model and seeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,13 +5,15 @@ const db = require('../../config/mongoose')
 
 const Category = require('../category')
 
-const categories = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
-const icons = ['fas fa-home', 'fas fa-shuttle-van', 'fas fa-grin-beam', 'fas fa-utensils', 'fas fa-pen']
+const categories = [
+  { name: '家居物業', name_en: 'home', icon: 'fas fa-home' },
+  { name: '交通出行', name_en: 'transportation', icon: 'fas fa-shuttle-van' },
+  { name: '休閒娛樂', name_en: 'entertainment', icon: 'fas fa-grin-beam' },
+  { name: '餐飲食品', name_en: 'food', icon: 'fas fa-utensils' },
+  { name: '其他', name_en: 'other', icon: 'fas fa-pen' }
+]
 
 db.once('open', async () => {
-  for (let i = 0; i < categories.length; i++) {
-    await Category.create({ name: categories[i], icon: icons[i] })
-  }
-  console.log('Categories done')
+  await Category.create(categories).then(() => console.log('Categories done'))
   process.exit()
 })
